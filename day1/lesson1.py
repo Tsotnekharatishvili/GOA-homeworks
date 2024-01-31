3a5fbf7edca673eec02dc3ac5b6c16da613eaed6
Tsotnekharatishvili <tsotnekharatishvili4@gmail.com>
2024-01-31 22:48:04 +0400

    first commit

---
+++ day1/lesson1.py
@@ -0,0 +1,8 @@
+name="tsotne"
+surname="kharatishvili"
+age="12"
+heigh="165.5"
+knows_pley_football="talse"
+is_stupid="false"
+print(name + " " + surname + " " + age + " " + heigh + " " + knows_pley_football + " " + is_stupid)
+
