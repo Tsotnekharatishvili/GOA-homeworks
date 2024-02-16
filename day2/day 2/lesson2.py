e12fd5f0f412096d8455bc12e76770a023bf445c
Tsotnekharatishvili <tsotnekharatishvili4@gmail.com>
2024-02-16 20:07:23 +0400

    first commit

---
+++ day2/day 2/lesson2.py
@@ -0,0 +1,10 @@
+name="leqso"
+surname="leverashvili"
+age= "14"
+heigh= "160.5"
+loves_football= "true"
+print (name + " " + surname + " " + age + " " + heigh + " " + loves_football)
+
+goa_is_best_academy= "true" 
+
+print(goa_is_best_academy)
